48c630c88c85b287d394ae3193c6ac229e94020c
nonoroazoro <xiaochao.k@gmail.com>
2018-04-01 16:30:05 +0800

    add declaration for css-modules

---
+++ src/typings/css-modules.ts
@@ -0,0 +1,5 @@
+// Declaration for css-modules, see https://stackoverflow.com/questions/48142521/ts-loader-css-loader-not-being-able-to-import-resolve-file/48154626#48154626
+declare module "*.less" {
+    const styles: { [key: string]: string };
+    export = styles;
+}
