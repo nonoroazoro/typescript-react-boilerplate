1e518e937ed8c42ee2e873418c5f6a042a85d0a2
nonoroazoro <xiaochao.k@gmail.com>
2018-04-02 20:22:23 +0800

    add click

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -21,18 +21,33 @@ export interface IState
     /**
      * Value.
      */
-    value: string;
+    value: number;
 }
 
 export default class App extends PureComponent<IProps, IState>
 {
+    state = {
+        value: 0
+    };
+
+    handleClick = () =>
+    {
+        this.setState((state) =>
+        {
+            return { value: state.value + 1 }
+        });
+    }
+
     render()
     {
         const { message, sender } = this.props;
+        const { value } = this.state;
         return (
             <div className={styles.app}>
                 <p>{`Message: ${message}`}</p>
                 <p>{`From ${sender}`}</p>
+                <p>{`Clicked ${value}`}</p>
+                <button type="button" onClick={this.handleClick}>Click Me</button>
             </div>
         );
     }
