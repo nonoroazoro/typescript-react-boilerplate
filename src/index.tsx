30f38440c3c43dd9daec2d34764891a445dc2674
nonoroazoro <xiaochao.k@gmail.com>
2018-04-02 20:12:57 +0800

    revert "remove react-hot-loader"
    This reverts commit 0d07e5033c492175ddbaeff53372fbf658255d46.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { AppContainer } from "react-hot-loader";
 
 import App from "./components/App";
 
 import "./index.less";
 
-ReactDOM.render(
-    <App message="Hello World" sender="Jack" />,
-    document.getElementById("root")
-);
+function render()
+{
+    ReactDOM.render(
+        <AppContainer>
+            <App message="Hello World" sender="Jack" />
+        </AppContainer>,
+        document.getElementById("root")
+    );
+}
+
+render();
+
+// HMR to preserve React's state, see https://github.com/gaearon/react-hot-loader/blob/master/docs/Troubleshooting.md
+declare const module: any;
+if (module.hot)
+{
+    module.hot.accept("./components/App", render);
+}
