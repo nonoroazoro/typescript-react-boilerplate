0d07e5033c492175ddbaeff53372fbf658255d46
nonoroazoro <xiaochao.k@gmail.com>
2018-04-02 17:20:56 +0800

    remove react-hot-loader

--- scripts/webpack/webpack.config.dev.js
+++ scripts/webpack/webpack.config.dev.js
@@ -1,4 +1,3 @@
-const webpack = require("webpack");
 const config = require("./webpack.config.base");
 
 config.mode = "development";
@@ -30,20 +29,6 @@ config.module.rules.push(
         ],
         exclude: /node_modules/
     }
-);
-
-// Hot module replacement, see http://gaearon.github.io/react-hot-loader/getstarted/
-for (const key of Object.keys(config.entry))
-{
-    config.entry[key].unshift(
-        "react-hot-loader/patch",
-        "webpack-dev-server/client?http://0.0.0.0:8080",
-        "webpack/hot/only-dev-server"
-    );
-}
-config.plugins.push(
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
 );
 
 module.exports = config;
--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
 
 import App from "./components/App";
 
 import "./index.less";
 
-function render()
-{
-    ReactDOM.render(
-        <AppContainer>
-            <App message="Hello World" sender="Jack" />
-        </AppContainer>,
-        document.getElementById("root")
-    );
-}
-
-render();
-
-// HMR to preserve React's state, see https://github.com/gaearon/react-hot-loader/blob/master/docs/Troubleshooting.md
-declare const module: any;
-if (module.hot)
-{
-    module.hot.accept("./components/App", render);
-}
+ReactDOM.render(
+    <App message="Hello World" sender="Jack" />,
+    document.getElementById("root")
+);
