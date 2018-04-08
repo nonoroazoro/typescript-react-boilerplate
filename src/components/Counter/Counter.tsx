9776a927d8ab2afced57e015bc3ec0f058f1a5ac
nonoroazoro <xiaochao.k@gmail.com>
2018-04-08 17:42:06 +0800

    adjust styles

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -22,13 +22,13 @@ const Counter = (props: CounterProps) =>
     const { className, actions: { increase, decrease, increaseAsync, decreaseAsync }, value } = props;
     return (
         <div className={cs(className, styles.container)}>
-            <span>{value}</span>
-            <button onClick={() => increase(1)}>+1</button>
-            <button onClick={() => decrease(1)}>-1</button>
-            <button onClick={() => increase(2)}>+2</button>
-            <button onClick={() => decrease(3)}>-3</button>
-            <button onClick={() => increaseAsync(1)}>Asynchronous Increment</button>
-            <button onClick={() => decreaseAsync(1)}>Asynchronous Decrement</button>
+            <span className={styles.value}>{value}</span>
+            <button className={styles.btn} onClick={() => increase(1)}>+1</button>
+            <button className={styles.btn} onClick={() => decrease(1)}>-1</button>
+            <button className={styles.btn} onClick={() => increase(2)}>+2</button>
+            <button className={styles.btn} onClick={() => decrease(3)}>-3</button>
+            <button className={styles.btn} onClick={() => increaseAsync(1)}>Asynchronous Increment</button>
+            <button className={styles.btn} onClick={() => decreaseAsync(1)}>Asynchronous Decrement</button>
         </div>
     );
 };
