68dc1ab4f49a665302f647476121b0c935732841
nonoroazoro <xiaochao.k@gmail.com>
2018-04-10 11:47:35 +0800

    add comment

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -4,6 +4,7 @@ import { RootState } from "../rootReducer";
 import configureStoreDev from "./configureStore.dev";
 import configureStoreProd from "./configureStore.prod";
 
+// The following code will be optimized by `Tree Shaking`.
 let configureStore: (preloadedState?: RootState) => Store<RootState>;
 if (process.env.NODE_ENV === "production")
 {
