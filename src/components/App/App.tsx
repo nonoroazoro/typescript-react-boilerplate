2f1c3f072f0cd15b55483d610efd321e87c562a3
nonoroazoro <xiaochao.k@gmail.com>
2018-04-11 12:25:49 +0800

    upgrade to react-hot-loader's new hot api

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { PureComponent } from "react";
+import { hot } from "react-hot-loader";
 
 import Counter from "../Counter";
 
 import * as styles from "./App.less";
 
+// Add hook to auto re-render the root component, see https://github.com/gaearon/react-hot-loader#appcontainer-vs-hot
+@hot(module)
 export default class App extends PureComponent
 {
     render()
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { AppContainer } from "react-hot-loader";
 import { Provider } from "react-redux";
 
 import App from "./components/App";
@@ -10,31 +9,9 @@ import "./index.less";
 
 const store = configureStore();
 
-function render()
-{
-    if (process.env.NODE_ENV !== "production")
-    {
-        // Add react-hot-loader to preserve state (to support React v16.3.0).
-        // See https://github.com/gaearon/react-hot-loader/issues/923
-        require("react-hot-loader").default.register(App, "App", module.id);
-    }
-
-    ReactDOM.render(
-        <Provider store={store}>
-            <AppContainer>
-                <App />
-            </AppContainer>
-        </Provider>,
-        document.getElementById("root")
-    );
-}
-
-render();
-
-// Add hook to auto re-render the root component.
-// See https://github.com/gaearon/react-hot-loader/blob/master/docs/Troubleshooting.md
-declare const module: any;
-if (module.hot)
-{
-    module.hot.accept("./components/App", render);
-}
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
