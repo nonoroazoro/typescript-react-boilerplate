a650ea8af7fe22452a9172585ad8f317e8b3930d
nonoroazoro <xiaochao.k@gmail.com>
2018-04-11 17:58:48 +0800

    enable verbose

--- scripts/jest/jest.config.js
+++ scripts/jest/jest.config.js
@@ -14,5 +14,6 @@ module.exports = {
     "setupTestFrameworkScriptFile": "<rootDir>/scripts/jest/jest.setup.js",
     "snapshotSerializers": ["enzyme-to-json/serializer"],
     "testRegex": "__tests__\\/.+\\.(test|spec)\\.tsx?$",
-    "transform": { "^.+\\.tsx?$": "ts-jest" }
+    "transform": { "^.+\\.tsx?$": "ts-jest" },
+    "verbose": true
 };
