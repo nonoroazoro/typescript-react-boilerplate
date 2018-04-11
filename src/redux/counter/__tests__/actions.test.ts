5736c9ea797a63078e70e395001c0ae2723f0c6c
nonoroazoro <xiaochao.k@gmail.com>
2018-04-11 15:59:16 +0800

    fulfill the tests of counter actions

---
+++ src/redux/counter/__tests__/actions.test.ts
@@ -0,0 +1,72 @@
+import * as configureStore from "redux-mock-store";
+import * as reduxPromiseMiddleware from "redux-promise";
+
+import { actionCreators, actionTypes } from "../index";
+
+let store: any;
+const createMockStore = configureStore([reduxPromiseMiddleware]);
+
+describe("Actions: counter", () =>
+{
+    beforeAll(() =>
+    {
+        store = createMockStore();
+    });
+
+    beforeEach(() =>
+    {
+        store.clearActions();
+    });
+
+    test("dispatch increase action", () =>
+    {
+        const amount = 1;
+        const expectedActions = [{
+            type: actionTypes.increase,
+            payload: amount
+        }];
+
+        const action = actionCreators.increase(amount);
+        store.dispatch(action);
+        expect(store.getActions()).toEqual(expectedActions);
+    });
+
+    test("dispatch decrease action", () =>
+    {
+        const amount = 1;
+        const expectedActions = [{
+            type: actionTypes.decrease,
+            payload: amount
+        }];
+
+        const action = actionCreators.decrease(amount);
+        store.dispatch(action);
+        expect(store.getActions()).toEqual(expectedActions);
+    });
+
+    test("dispatch increaseAsync action", async () =>
+    {
+        const amount = 1;
+        const expectedActions = [{
+            type: actionTypes.increaseAsync,
+            payload: amount
+        }];
+
+        const action = actionCreators.increaseAsync(amount);
+        await store.dispatch(action);
+        expect(store.getActions()).toEqual(expectedActions);
+    });
+
+    test("dispatch decreaseAsync action", async () =>
+    {
+        const amount = 1;
+        const expectedActions = [{
+            type: actionTypes.decreaseAsync,
+            payload: amount
+        }];
+
+        const action = actionCreators.decreaseAsync(amount);
+        await store.dispatch(action);
+        expect(store.getActions()).toEqual(expectedActions);
+    });
+});
