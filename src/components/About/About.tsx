8ad45db219280b1c920728900c76f691da366b89
nonoroazoro <xiaochao.k@gmail.com>
2018-04-12 17:33:07 +0800

    add about component

---
+++ src/components/About/About.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import * as styles from "./About.less";
+
+/**
+ * About Page.
+ */
+export default () =>
+{
+    return (
+        <div className={styles.circle}>
+            <h3 className={styles.title}>For the Horde!</h3>
+        </div>
+    );
+};
---
+++ src/components/About/index.ts
@@ -0,0 +1,2 @@
+export * from "./About";
+export { default } from "./About";
