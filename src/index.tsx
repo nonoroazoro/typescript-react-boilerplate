a1c1b8b6f2b0e78ca8b03d81b4899f646372b406
nonoroazoro <xiaochao.k@gmail.com>
2019-03-06 18:10:39 +0800

    fix router

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
+import { createBrowserHistory } from "history";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Router } from "react-router";
 
 import { App } from "./components/App";
 
 import "./index.less";
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
-    <App />,
+    <Router history={history}>
+        <App />
+    </Router>,
     document.getElementById("root")
 );
