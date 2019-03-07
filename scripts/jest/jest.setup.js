c82e8da0e80bfa7f704a65032ae44e1b3d8e9e60
nonoroazoro <xiaochao.k@gmail.com>
2019-03-07 10:06:49 +0800

    remove raf

--- scripts/jest/jest.setup.js
+++ scripts/jest/jest.setup.js
@@ -1,8 +1,5 @@
 require("jest-fetch-mock");
 
-// see https://github.com/facebook/create-react-app/issues/3199
-require("raf/polyfill");
-
 const { configure } = require("enzyme");
 const Adapter = require("enzyme-adapter-react-16");
 configure({ adapter: new Adapter() });
