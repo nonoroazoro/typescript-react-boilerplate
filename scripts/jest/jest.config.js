5bcb731f4c69c79f803702f6a5a13157cd2c65f0
nonoroazoro <xiaochao.k@gmail.com>
2019-04-29 10:49:11 +0800

    rename to tests

--- scripts/jest/jest.config.js
+++ scripts/jest/jest.config.js
@@ -6,14 +6,14 @@ module.exports = {
             "tsConfig": "tsconfig.test.json"
         }
     },
-    "moduleFileExtensions": ["js", "ts", "tsx"],
+    "moduleFileExtensions": ["js", "jsx", "ts", "tsx"],
     "moduleNameMapper": {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/scripts/jest/__mocks__/fileMock.js",
         "\\.(css|less)$": "identity-obj-proxy"
     },
     "setupFiles": ["<rootDir>/scripts/jest/jest.setup.js"],
     "snapshotSerializers": ["enzyme-to-json/serializer"],
-    "testRegex": "/__tests__/.+\\.(test|spec)\\.tsx?$",
+    "testRegex": "/tests/.+\\.(test|spec)\\.tsx?$",
     "transform": { "^.+\\.tsx?$": "ts-jest" },
     "verbose": true
 };
