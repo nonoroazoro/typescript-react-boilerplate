e3bf67562dc5bdb016b4e4d5da38f8b1b669b331
nonoroazoro <xiaochao.k@gmail.com>
2019-04-30 16:41:10 +0800

    add dev environments

--- scripts/utils/env.js
+++ scripts/utils/env.js
@@ -3,6 +3,10 @@ const path = require("path");
 
 const rootPath = fs.realpathSync(process.cwd());
 module.exports = {
+    devProtocol: "http",
+    devHost: process.env.HOST || "0.0.0.0",
+    devPort: process.env.PORT || 8080,
+
     rootPath,
     buildPath: path.resolve(rootPath, "./dist"),
     faviconPath: path.resolve(rootPath, "./template/favicon.ico"),
