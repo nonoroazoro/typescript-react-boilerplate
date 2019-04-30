55ca7effe66812fee4e97295b7c3d23203049e2c
nonoroazoro <xiaochao.k@gmail.com>
2019-04-30 16:57:13 +0800

    add isProd and refator htmlPath

--- scripts/utils/env.js
+++ scripts/utils/env.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
+const isProd = process.env.NODE_ENV === "production";
 const rootPath = fs.realpathSync(process.cwd());
 module.exports = {
+    isProd,
+
     devProtocol: "http",
     devHost: process.env.HOST || "0.0.0.0",
     devPort: process.env.PORT || 8080,
@@ -10,5 +13,8 @@ module.exports = {
     rootPath,
     buildPath: path.resolve(rootPath, "./dist"),
     faviconPath: path.resolve(rootPath, "./template/favicon.ico"),
-    htmlPath: path.resolve(rootPath, "./template/index.html")
+    htmlPath: path.resolve(
+        rootPath,
+        `./template/${isProd ? "index-prod" : "index-dev"}.html`
+    )
 };
