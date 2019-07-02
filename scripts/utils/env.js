745a7a86fb2fc1270e99940018da283f77a1b15b
nonoroazoro <xiaochao.k@gmail.com>
2019-07-02 11:08:48 +0800

    adjust order

--- scripts/utils/env.js
+++ scripts/utils/env.js
@@ -5,12 +5,11 @@ const isProd = process.env.NODE_ENV === "production";
 const rootPath = fs.realpathSync(process.cwd());
 module.exports = {
     isProd,
+    rootPath,
 
     devProtocol: "http",
     devHost: process.env.HOST || "0.0.0.0",
     devPort: process.env.PORT || 8080,
-
-    rootPath,
     buildPath: path.resolve(rootPath, "./dist"),
     faviconPath: path.resolve(rootPath, "./template/favicon.ico"),
     htmlPath: path.resolve(
