bee2030d92cfdff15e3c435e861f7fab64f56e45
Nonoroazoro <xiaochao.k@gmail.com>
2020-10-12 17:08:07 +0800

    add UnionToIntersection

--- src/typings/utils.d.ts
+++ src/typings/utils.d.ts
@@ -29,3 +29,8 @@ export type Properties<T, Type> = Pick<T, PropertyNames<T, Type>>;
  * From `T`, extracts a set of properties those types are `not` assignable to `Type`.
  */
 export type ExcludeProperties<T, Type> = Pick<T, ExcludePropertyNames<T, Type>>;
+
+/**
+ * Converts union type into Intersection type.
+ */
+type UnionToIntersection<U> = (U extends any ? (arg: U) => void : never) extends ((arg: infer I) => void) ? I : never;
