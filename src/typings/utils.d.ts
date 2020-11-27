28fe0c15e226e40b450589ffc557f7aa2daef8a8
Nonoroazoro <xiaochao.k@gmail.com>
2020-11-27 16:36:52 +0800

    remove export

--- src/typings/utils.d.ts
+++ src/typings/utils.d.ts
@@ -4,7 +4,7 @@
  *
  * Use TypedProperties<T, Type, `true`> to reverse the result (exclude the `Type`).
  */
-export type TypedProperties<T, Type, Reversed = false> = {
+type TypedProperties<T, Type, Reversed = false> = {
     [Key in keyof T]-?: Exclude<T[Key], undefined> extends Type
     ? (Reversed extends false ? Key : never)
     : (Reversed extends false ? never : Key)
@@ -13,29 +13,29 @@ export type TypedProperties<T, Type, Reversed = false> = {
 /**
  * From `T`, extracts property names those types are assignable to `Type`.
  */
-export type PropertyNames<T, Type> = TypedProperties<T, Type>[keyof T];
+type PropertyNames<T, Type> = TypedProperties<T, Type>[keyof T];
 
 /**
  * From `T`, extracts property names those types are `not` assignable to `Type`.
  */
-export type ExcludePropertyNames<T, Type> = TypedProperties<T, Type, true>[keyof T];
+type ExcludePropertyNames<T, Type> = TypedProperties<T, Type, true>[keyof T];
 
 /**
  * From `T`, extracts a set of properties those types are assignable to `Type`.
  */
-export type Properties<T, Type> = Pick<T, PropertyNames<T, Type>>;
+type Properties<T, Type> = Pick<T, PropertyNames<T, Type>>;
 
 /**
  * From `T`, extracts a set of properties those types are `not` assignable to `Type`.
  */
-export type ExcludeProperties<T, Type> = Pick<T, ExcludePropertyNames<T, Type>>;
+type ExcludeProperties<T, Type> = Pick<T, ExcludePropertyNames<T, Type>>;
 
 /**
  * Converts enum type into union type (TypeScript 4.1+).
  */
-export type EnumToUnion<E> = `${E}`;
+type EnumToUnion<E> = `${E}`;
 
 /**
  * Converts union type into intersection type.
  */
-export type UnionToIntersection<U> = (U extends any ? (arg: U) => void : never) extends ((arg: infer I) => void) ? I : never;
+type UnionToIntersection<U> = (U extends any ? (arg: U) => void : never) extends ((arg: infer I) => void) ? I : never;
