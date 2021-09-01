9cfcd02092069b32a4987202b0a9e7cf1ea81162
Nonoroazoro <xiaochao.k@gmail.com>
2021-09-01 17:29:46 +0800

    tweak eslint configs

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+const { isProd } = require("./scripts/build.config");
+
+const config = {
+    "extends": [
+        "eslint-config-zoro"
+    ],
+    "env": {
+        "jest": true
+    },
+    "parserOptions": {
+        "project": "./tsconfig.eslint.json"
+    }
+};
+
+if (!isProd)
+{
+    config.rules = {
+        "no-console": "off",
+        "@typescript-eslint/no-unused-vars": "off"
+    };
+}
+
+module.exports = config;
