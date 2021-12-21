cbdcfec7fe35c7487e70a79d9cd33c150c169afe
Nonoroazoro <xiaochao.k@gmail.com>
2021-12-21 10:46:35 +0800

    add types

--- src/utils/fp.ts
+++ src/utils/fp.ts
@@ -1,20 +1,27 @@
-export function compose(...fns: Array<(...args: any[]) => any>)
+export type ArityFunction = (...args: any) => any;
+export type UnaryFunction = (arg: any) => any;
+
+export function compose(...fns: [...unary: UnaryFunction[], arity: ArityFunction])
 {
     if (fns.length === 0)
     {
         throw new Error("compose requires at least one argument");
     }
-    return pipe(...[...fns].reverse());
+    const fn = fns.pop() as ArityFunction;
+    return (...args: Parameters<ArityFunction>) =>
+    {
+        return fns.reduceRight((v, f) => f(v), fn(...args));
+    };
 }
 
-export function pipe(...fns: Array<(...args: any[]) => any>)
+export function pipe(...fns: [arity: ArityFunction, ...unary: UnaryFunction[]])
 {
     if (fns.length === 0)
     {
         throw new Error("pipe requires at least one argument");
     }
     const [f0, ...rest] = fns;
-    return (...args: Parameters<typeof f0>) =>
+    return (...args: Parameters<ArityFunction>) =>
     {
         return rest.reduce((v, f) => f(v), f0(...args));
     };
