fef24f2577d1d7d6a5d0fa74fe9bc069326bf320
Nonoroazoro <xiaochao.k@gmail.com>
2022-04-08 17:55:09 +0800

    migrate to createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
-import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 
-render(
-    (
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    ),
-    document.getElementById("root")
+createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
