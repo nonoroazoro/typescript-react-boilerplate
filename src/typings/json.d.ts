2e722f83e5a5d9b10936f065b226411e736c49aa
Nonoroazoro <xiaochao.k@gmail.com>
2022-07-07 15:34:43 +0800

    fix typings

--- src/typings/json.d.ts
+++ src/typings/json.d.ts
@@ -1,16 +1,21 @@
 /**
+ * JSON scalar.
+ */
+type JSONScalar = boolean | number | string | null;
+
+/**
+ * JSON value.
+ */
+type JSONValue = JSONArray | JSONObject | JSONScalar;
+
+/**
  * JSON object.
  */
 type JSONObject = {
-    [key in string]?: JsonArray | JsonObject | JSONScalar
+    [key in string]?: JSONValue
 };
 
 /**
  * JSON array.
  */
-type JSONArray = Array<JsonArray | JsonObject | JSONScalar>;
-
-/**
- * JSON scalar.
- */
-type JSONScalar = boolean | number | string | null;
+interface JSONArray extends Array<JSONValue> { }
